fc7f1908cc66fb73a1693977171b81ba84818977
Alexey Kharitonov <alexeywest024@list.ru>
2023-08-03 13:08:12 +0300

    endpoint

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
 app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
@@ -25,7 +24,6 @@ const PORT = config.get("port") ?? 8080;
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "client")));
-
   const indexPath = path.join(__dirname, "client", "index.html");
 
   app.get("*", (req, res) => {
